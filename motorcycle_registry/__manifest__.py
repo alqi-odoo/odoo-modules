0f25262429d96e5dbc8950b6162ec9b2ec79b9de
Albert Qi <alqi@odoo.com>
2023-06-13 14:05:55 -0400

    Update capitalization

--- motorcycle_registry/__manifest__.py
+++ motorcycle_registry/__manifest__.py
@@ -1,9 +1,9 @@
 {
     "name": "Motorcycle Registry",
-    "summary": "Manage Registration of Motorcycles",
+    "summary": "Manage registration of motorcycles",
     "description": """Motorcycle Registry
 ====================
-This module is used to keep track of the Motorcycle Registration and Ownership of each motorcycle of the brand.""",
+This module is used to keep track of the motorcycle registration and ownership of each motorcycle of the brand.""",
     "author": "alqi-odoo",
     "website": "https://github.com/alqi-odoo/odoo-modules",
     "category": "Kawiil",
