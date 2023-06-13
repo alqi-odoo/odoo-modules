30552450eff2e55a958f68480dc7f28bace576c1
Albert Qi <alqi@odoo.com>
2023-06-13 13:54:45 -0400

    Registry

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Motorcycle Registry",
+    "summary": "Manage Registration of Motorcycles",
+    "description": """Motorcycle Registry
+====================
+This module is used to keep track of the Motorcycle Registration and Ownership of each motorcycle of the brand.""",
+    "author": "alqi-odoo",
+    "website": "https://github.com/alqi-odoo/motorcycle-registry",
+    "category": "Kawiil",
+    "application": True,
+}
