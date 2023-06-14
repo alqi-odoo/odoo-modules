0d9537a8458f442ef7611cdfdc66966216c9ae67
Albert Qi <alqi@odoo.com>
2023-06-14 10:33:44 -0400

    Add fields to motorcycle registry model

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -1,5 +1,15 @@
-from odoo import models
+from odoo import fields, models
 
 
 class MotorcycleRegistry(models.Model):
     _name = "motorcycle.registry"
+
+    registry_number = fields.Char(string="Registry Number", required=True)
+    vin = fields.Char(string="VIN", required=True)
+    first_name = fields.Char(string="First Name", required=True)
+    last_name = fields.Char(string="Last Name", required=True)
+    picture = fields.Image(string="Picture")
+    current_mileage = fields.Float(string="Current Mileage")
+    license_plate = fields.Char(string="License Plate")
+    certificate_title = fields.Image(string="Certificate of Title")
+    register_date = fields.Date(string="Register Date")
